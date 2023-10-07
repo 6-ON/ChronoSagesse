f4ed91af13f9664c950b65a8f97a1a6960b26a92
6-ON <aminhatim6@gmail.com>
2023-10-07 16:49:41 +0100

    Add MySQL2 connection pool configuration

---
+++ db/index.js
@@ -0,0 +1,15 @@
+const mysql2 = require('mysql2/promise')
+const pool = mysql2.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'chronosagesse',
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+})
+
+module.exports = pool 
