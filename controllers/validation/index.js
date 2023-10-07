d8f3f99db55364bd062ced9d615c93b6e786dc5b
6-ON <aminhatim6@gmail.com>
2023-10-07 17:42:44 +0100

    blog CU, category CU schemas

---
+++ controllers/validation/index.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+// Schema for adding a new blog
+exports.addBlogSchema = Joi.object({
+    title: Joi.string().required(),
+    image: Joi.string().required(),
+    content: Joi.string().required().messages({
+        'string.empty': 'Content is required',
+    }),
+    categories: Joi.array().items(Joi.string()).required(),
+});
+
+// Schema for updating an existing blog
+exports.updateBlogSchema = Joi.object({
+    id: Joi.number().required(),
+    title: Joi.string(),
+    image: Joi.string(),
+    content: Joi.string().messages({
+        'string.empty': 'Content is required',
+    }),
+    categories: Joi.array().items(Joi.string()),
+});
+
+exports.addCategorySchema = Joi.object({
+    name: Joi.string().required(),
+});
+exports.updateCategorySchema = Joi.object({
+    id: Joi.number().required(),
+    name: Joi.string().required(),
+});
+// (async()=>console.log(await this.updateBlogSchema.validateAsync({id:5,image:''})))();
