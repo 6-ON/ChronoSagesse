b8b4fb7649be87f2c8304c0c9f1d6ffe01780b91
6-ON <aminhatim6@gmail.com>
2023-10-07 16:36:57 +0100

    Add session, flash-connect, cookie-parser, layouts

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express'),
     app = express()
+const expressLayouts = require('express-ejs-layouts')
+const flash = require('connect-flash')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+app.use(express.static('public'))
+app.use(cookieParser('keyboard cat'))
+app.use(session({ cookie: { maxAge: 60000 } }))
+app.use(flash())
 
+app.use(expressLayouts)
+app.set('layout', './layouts/default')
 app.set('view engine', 'ejs')
-
-app.get('/', function (req, res) {
-    res.render('index')
-})
-
-
+app.use('/', require('./routes'))
 app.listen(8080, function () {
     console.log('Server is running on port 8080 ')
 })
