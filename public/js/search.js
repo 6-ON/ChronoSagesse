865a3d3a1ddc51412968c4528376b955cc4a1489
6-ON <aminhatim6@gmail.com>
2023-10-07 17:35:42 +0100

    add title search script

---
+++ public/js/search.js
@@ -0,0 +1,15 @@
+const searchInput = document.querySelector('#search-input');
+const searchLink = document.querySelector('#search-link');
+
+searchInput.addEventListener('input', () => {
+    const value = searchInput.value;
+    const url = new URL(window.location.href);
+    url.searchParams.set('title', value);
+    searchLink.href = url;
+});
+searchInput.addEventListener('keyup', (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        searchLink.click();
+    }
+});
