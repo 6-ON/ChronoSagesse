a0c2403a7504ba9345048db9278a7c4242ccb9aa
6-ON <aminhatim6@gmail.com>
2023-10-07 17:41:41 +0100

    wrap all the controllers into one file

---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+exports.blogsControler = require('../controllers/blogs')
+exports.homeController = require('../controllers/home')
+exports.categoriesController = require('../controllers/categories')
