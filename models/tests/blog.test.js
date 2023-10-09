0b7a10c847f639e8c05966ceb375c58c9ecd2465
6-ON <aminhatim6@gmail.com>
2023-10-09 01:20:44 +0100

    create blog create update tests

---
+++ models/tests/blog.test.js
@@ -0,0 +1,39 @@
+const db = require('../../db')
+const blog = require('../blog')
+
+describe('Blog model', () => {
+    it('should add a new blog post to the database', async () => {
+        const newBlog = {
+            title: 'Test Blog Post',
+            image: 'test.jpg',
+            content: 'This is a test blog post.',
+            categories: [1, 2, 3],
+        }
+
+        const addedBlog = await blog.add(newBlog)
+
+        expect(addedBlog).toHaveProperty('id')
+        expect(addedBlog.title).toBe(newBlog.title)
+        expect(addedBlog.image).toBe(newBlog.image)
+        expect(addedBlog.content).toBe(newBlog.content)
+        expect(addedBlog.category).toEqual(newBlog.category)
+    })
+
+    it('should retrieve a blog post from the database by ID', async () => {
+        const newBlog = {
+            title: 'Test Blog Post',
+            image: 'test.jpg',
+            content: 'This is a test blog post.',
+            categories: [1, 2, 3],
+        }
+
+        const addedBlog = await blog.add(newBlog)
+        const retrievedBlog = await blog.get(addedBlog.id)
+
+        expect(retrievedBlog).toHaveProperty('id')
+        expect(retrievedBlog.title).toBe(newBlog.title)
+        expect(retrievedBlog.image).toBe(newBlog.image)
+        expect(retrievedBlog.content).toBe(newBlog.content)
+        expect(retrievedBlog.category).toEqual(newBlog.category)
+    })
+})
